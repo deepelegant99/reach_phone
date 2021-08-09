91bf1acdc15bb5bd5b921ea7dc1d3a92e979bfff
Henry Crossley <crossh2001@yahoo.com>
2021-08-08 21:27:07 -0700

    fixed button function

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,35 +6,29 @@ import { findAllInRenderedTree } from "react-dom/test-utils";
 const Blt= ()=>{
 
     const [button, setButton]=useState("");
-    let clicked="off";
 
     function attach(num){
         //button=1;
         console.log(num);
-        //alert("Hi player "+num);
+        alert("Hi player "+num);
         //setButton(num);
     }
 
-    function c(clicked, num){
-        if(clicked="on")
-        {
-            attach(num);
-        }
-    }
+   
 
     return(
  
         <div className="numbers">
-            <button className="b1" onClick={() => { this.props.attach(1) }}>1</button>
-            <button onClick="">2</button>
-            <button onClick="">3</button>
-            <br/><button onClick="">4</button>
-            <button onClick="">5</button>
-            <button onClick="">6</button>
-            <button onClick="">7</button>
-            <br/><button onClick="">8</button>
-            <button onClick="">9</button>
-            <button onClick="">0</button><br/>
+            <button value="1" className="b1" onClick={() => { attach(1) }}>1</button>
+            <button onClick={() => { attach(2) }}>2</button>
+            <button onClick={() => { attach(3) }}>3</button>
+            <br/><button onClick={() => { attach(4) }}>4</button>
+            <button onClick={() => { attach(5) }}>5</button>
+            <button onClick={() => { attach(6) }}>6</button>
+            <button onClick={() => { attach(7) }}>7</button>
+            <br/><button onClick={() => { attach(8) }}>8</button>
+            <button onClick={() => { attach(9) }}>9</button>
+            <button onClick={() => { attach(0) }}>0</button><br/>
             <button>ENTER</button>
             </div>
     );
